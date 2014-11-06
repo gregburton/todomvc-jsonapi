bf688a301f474425be9780fb51b7057cf4eba984
gregurton <gburt802@gmail.com>
2014-11-05 22:43:20 -0500

    behvior of app seems fine until I restart the rails server. Issues include: items that are supposed to be deleted are having duplicates created, and the completed boolean is not being switched from false to true.

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    @todo =Todo.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @todo }
+    end
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: api_v1_todo_path(@todo)
+    else
+      render @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todo_params)
+      head :no_content
+    else
+      render @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :order, :completed)
+  end
 end
--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,23 +1,23 @@
 class TodoSerializer < ActiveModel::Serializer
-  attributes :id, :title, :order, :completed, :created_at, :updated at
+  attributes :id, :title, :order, :completed, :created_at, :updated_at
 
-  def title
-    "#{object.title}"
-  end
-
-  def order
-    "#{object.order}"
-  end
-
-  def completed
-    "#{object.completed}"
-  end
-
-  def created_at
-    object.created_at.to_i
-  end
-
-  def updated_at
-    object.updated_at.to_i
-  end
+  # def title
+  #   "#{object.title}"
+  # end
+  #
+  # def order
+  #   "#{object.order}"
+  # end
+  #
+  # def completed
+  #   "#{object.completed}"
+  # end
+  #
+  # def created_at
+  #   object.created_at.to_i
+  # end
+  #
+  # def updated_at
+  #   object.updated_at.to_i
+  # end
 end
